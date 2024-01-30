fab24826c605119ed5a54aa00b25d959738ecc2b
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-01-30 15:40:41 -0500

    try new approach

--- pr.sh
+++ pr.sh
@@ -22,11 +22,11 @@ if [ "$PR_NUMBER" == "null" ]; then
   exit 1
 fi
 
-# Fetch current pull request details
+# # Fetch current pull request details
 CURRENT_BODY=$(gh pr view $PR_NUMBER --json body -q .body)
 
 # Check if newBody already exists in the current description
-if ! echo "$CURRENT_BODY" | jq --exit-status --arg BODY "$BODY" 'if .body != null then .body | index($BODY) else false end' > /dev/null; then
+if ! echo "$CURRENT_BODY" | grep -q "$BODY"; then
   echo "New body does not exist in the current description. Updating..."
   
   # Concatenate the new text to the existing description
@@ -37,8 +37,6 @@ if ! echo "$CURRENT_BODY" | jq --exit-status --arg BODY "$BODY" 'if .body != nul
 else
   echo "New body already exists in the current description. No update needed."
 fi
-
-
 
 # # Fetch current pull request details
 # CURRENT_BODY=$(gh pr view $PR_NUMBER --json body -q .body)
