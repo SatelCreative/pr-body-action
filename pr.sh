e1eb934d219315c6beaf2ba1ae75a72ba263a820
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-01-31 15:39:00 -0500

    back to grep

--- pr.sh
+++ pr.sh
@@ -27,8 +27,7 @@ echo "DEBUG: BODY=$BODY"
 echo "DEBUG: CURRENT_BODY=$CURRENT_BODY"
 
 # Use grep to check if the pattern in $BODY is found in $CURRENT_BODY
-#if awk -v RS='' -v ORS='\n\n' -v body="$BODY" 'BEGIN {if (index(ENVIRON["CURRENT_BODY"], body) > 0) exit 0; else exit 1}'; then
-if echo "$CURRENT_BODY" | awk -v var="$BODY" '$0 == var {exit 1}'; then
+if echo "$CURRENT_BODY" | grep -Fxq "$BODY"; then
   echo "New body already exists in the current description. No update needed."
 else
   echo "New body does not exist in the current description. Updating..."
