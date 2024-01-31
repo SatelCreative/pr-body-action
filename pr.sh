9a8ea592bac4d95b9d82c3e19e90089aa296bee1
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-01-31 13:48:03 -0500

    remove echos

--- pr.sh
+++ pr.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-echo "BODY=$BODY"
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "Error: GITHUB_TOKEN input is required"
   exit 1
@@ -15,7 +14,6 @@ fi
 
 # Get pull request number
 PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-echo "PR_NUMBER=$PR_NUMBER"
 
 if [ "$PR_NUMBER" == "null" ]; then
   echo "Error: No pull request found"
@@ -24,16 +22,6 @@ fi
 
 # Fetch current pull request details
 CURRENT_BODY=$(gh pr view $PR_NUMBER --json body -q .body)
-echo "CURRENT_BODY=$CURRENT_BODY"
-
-echo "DEBUG: CURRENT_BODY=$CURRENT_BODY"
-echo "DEBUG: BODY=$BODY"
-
-echo "DEBUG: Length of CURRENT_BODY=${#CURRENT_BODY}"
-echo "DEBUG: Length of BODY=${#BODY}"
-
-# Escape special characters in BODY
-# ESCAPED_BODY=$(echo "$BODY" | sed 's/[][\\.*^$/]/\\&/g')
 
 if echo "$CURRENT_BODY" | grep -q "$BODY"; then
   echo "New body already exists in the current description. No update needed."
@@ -42,7 +30,5 @@ else
   # Concatenate the new text to the existing description
   COMBINED_BODY="${CURRENT_BODY} ${BODY}"
   echo "Updated body: $COMBINED_BODY"
-  
-  # Uncomment the following line when you are ready to actually update the pull request
   gh pr edit $PR_NUMBER --body "${COMBINED_BODY}"
 fi
